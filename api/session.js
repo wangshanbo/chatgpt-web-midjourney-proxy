68091d0fe47fb1178001c1e34ca68b6b52cea87e
wangshanbo <627257359@qq.com>
2024-06-04 22:59:28 +0800

    feat：上传图片大小修改

--- api/session.js
+++ api/session.js
@@ -1,5 +1,4 @@
 module.exports = (req, res) => {
-  console.log('session.js', req.body);
   try {
     let data = req.body.data; 
     let obj ={
@@ -18,7 +17,7 @@ module.exports = (req, res) => {
             , notify : process.env.SYS_NOTIFY?? "" 
             ,disableGpt4 : process.env.DISABLE_GPT4?? "" 
             ,isWsrv:  process.env.MJ_IMG_WSRV?? "" 
-            ,uploadImgSize: process.env.UPLOAD_IMG_SIZE?? "1" 
+            ,uploadImgSize: process.env.UPLOAD_IMG_SIZE?? "5" 
             ,gptUrl : process.env.GPT_URL?? ""
             ,theme : process.env.SYS_THEME?? "dark"
             ,isCloseMdPreview : process.env.CLOSE_MD_PREVIEW?true:false
